66ff663241e2c2c6610c6ef76c7e9e560bf788f5
Ahmed Asem <asem4303@gmail.com>
2026-06-06 19:49:15 +0300

    01-Product  Module [Entities Building]

---
+++ ECommerce.Domain/Entities/ProductModule/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Entities.ProductModule
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; }=default!;
+        public string PictureUrl { get; set; } = default!;
+        public decimal  Price { get; set; }
+
+        public int ProductBrandId { get; set; }
+        public ProductBrand ProductBrand { get; set; } = null!;
+        public int ProductTypeId { get; set; }
+        public ProductType ProductType { get; set; }=null!;
+    }
+}
---
+++ ECommerce.Domain/Entities/ProductModule/ProductBrand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Entities.ProductModule
+{
+    public class ProductBrand:BaseEntity<int>
+    {
+        public string Name { get; set; } = default!;
+    }
+}
